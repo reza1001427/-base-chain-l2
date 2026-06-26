ffc54004663550b541a124ecffb4b8055c862456
reza1001427 <87238012+reza1001427@users.noreply.github.com>
2026-06-26 07:32:21 +0330

    Remove Hardhat configuration

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,12 +1,1 @@
-require("@nomicfoundation/hardhat-toolbox");
-require("dotenv").config();
-
-module.exports = {
-  solidity: "0.8.20",
-  networks: {
-    sepolia: {
-      url: process.env.ALCHEMY_URL,
-      accounts: [process.env.PRIVATE_KEY],
-    },
-  },
-};
+hardhat.config.js
