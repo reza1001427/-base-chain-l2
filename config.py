8fdac27729bdf2e90b69d1a4c31fc7fa46c97b4a
reza1001427 <87238012+reza1001427@users.noreply.github.com>
2026-06-26 07:38:30 +0330

    Create config.py

---
+++ config.py
@@ -0,0 +1,12 @@
+RPC_URL = "https://mainnet.infura.io/v3/YOUR_KEY"
+CONTRACT_ADDRESS = "0xYourContractAddress"
+
+CONTRACT_ABI = [
+    {
+        "inputs": [],
+        "name": "yourFunction",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    }
+]
